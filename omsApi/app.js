a8ca18bfe43268ff2ece376e4677a63d7764b99e
Frederico Bajouco <frederico.bajouco@gmail.com>
2024-11-13 15:27:55 +0000

    Remove unused views and routes; add virus model and API routes

--- omsApi/app.js
+++ omsApi/app.js
@@ -2,26 +2,35 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// var mongoose = require('mongoose');
-// mongoose.set('strictQuery', true);
-// mongoose.connect('');
+// MongoDB connection
+var mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+try {
+  mongoose.connect('mongodb+srv://u24t05:estaeapassword@upskilldb.hlsqn.mongodb.net/x_herois');
+  console.log(' ==> Connected to MongoDB');
+}
+catch (err) {
+  console.log(' ==> Error connecting to MongoDB');
+  console.log(err);
+}
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// API routes
+var virusRoutes = require('./routes/virus'); //rotas
+app.use('/api/virus', virusRoutes); //endpoint
 
 var port = 8080;
 app.listen(port);
 console.log("OMS-Api running in port " + port);
 
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 
-var indexRouter = require('./routes/index');
-app.use('/', indexRouter);
+// var indexRouter = require('./routes/index');
+// app.use('/', indexRouter);
 
 module.exports = app;
 
---
+++ omsApi/routes/virus.js
@@ -0,0 +1,21 @@
+var express = require('express'); 
+var router = express.Router();
+const DriverController = require('../controllers/virusController');
+
+router.post('/', virusController.createVirus);
+router.get('/', virusController.getAllVirus);
+
+module.exports = router;
+
+
+
+
+// router.post('/', DriverController.createDriver);
+// router.get('/', DriverController.getAllDrivers);
+// router.get('/id/:id', DriverController.getDriverById);
+// router.get('/name/:name', DriverController.getDriverByName);
+// router.get('/idcard/:idcard', DriverController.getDriverByIdCard);
+// router.get('/dl/:dl', DriverController.getDriverByLicence);
+// router.put('/:id',DriverController.updateDriver);
+// router.delete('/:id',DriverController.deleteDriver);
+
