7ad6b06b3d854387d15f4ad87c647d87183ad14a
Leila <127947556+leilatdantas@users.noreply.github.com>
2024-11-13 11:46:04 +0000

    architecture start

---
+++ omsApi/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// var mongoose = require('mongoose');
+// mongoose.set('strictQuery', true);
+// mongoose.connect('');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+var port = 8080;
+app.listen(port);
+console.log("OMS-Api running in port " + port);
+
+const cors = require('cors');
+app.use(cors());
+
+var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+module.exports = app;
+
